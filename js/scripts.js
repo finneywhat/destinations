4c28945ae267cdf4b8cd334f6d0a15f44d86cdc6
Chris Finney and Cameron Jensen <cameron@>
2017-02-27 10:31:16 -0800

    Add finishing touchs for required specs. Destination list shows all inputs when clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,37 @@
 // Business Logic
-function Destination(place,landmark,date,notes) {
-  this.place = place;
-  this.landmark = landmark;
-  this.date = date;
-  this.notes = notes;
+function Area(place, landmark, date, notes) {
+  this.place1 = place;
+  this.landmark1 = landmark;
+  this.date1 = date;
+  this.notes1 = notes;
 }
 
 //User Interface
 $(document).ready(function(){
-  $("#new-destination").sumbit(function(event){
-
+  $("form#new-destination").submit(function(event){
+    event.preventDefault();
+debugger;
     var newPlace = $("input#new-place").val();
     var newLandmark = $("input#new-landmark").val();
     var newDate = $("input#new-date").val();
     var newNotes = $("input#new-notes").val();
 
+    var newDestiny = new Area(newPlace, newLandmark, newDate, newNotes);
 
+    $("ul#destinations").append("<li><span class='clickable'>" + newDestiny.place1 + "</span></li>");
 
+    $(".clickable").last().click(function(){
+      $("#show-place").show();
+      $("#show-place h2").text(newDestiny.place1);
+      $(".new-place").text(newDestiny.place1);
+      $(".new-landmark").text(newDestiny.landmark1);
+      $(".new-date").text(newDestiny.date1);
+      $(".new-notes").text(newDestiny.notes1);
+
+    });
+    $("input#new-place").val("");
+    $("input#new-landmark").val("");
+    $("input#new-date").val("");
+    $("input#new-notes").val("");
   });
 });
